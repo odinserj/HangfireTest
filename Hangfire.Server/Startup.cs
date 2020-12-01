4dfcbde651e215dfdd77e6e5a21ffe5bdaa4a986
Sergey Odinokov <odinserj@gmail.com>
2020-12-01 16:13:24 +0500

    Switch to AssemblyLoadContext loading

--- Hangfire.Server/Startup.cs
+++ Hangfire.Server/Startup.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using System.Runtime.Loader;
 using System.Threading.Tasks;
 using Hangfire.SqlServer;
 using Interface;
@@ -100,10 +101,48 @@ namespace Hangfire.Server
             foreach (var file in allfiles)
             {
                 FileInfo info = new FileInfo(file);
-                plugInAssemblyList.Add(Assembly.LoadFrom(info.FullName));
+                var loadContext = new PluginLoadContext(info.FullName);
+
+                var assembly = loadContext.LoadFromAssemblyName(
+                    new AssemblyName(
+                        Path.GetFileNameWithoutExtension(info.FullName)));
+
+                plugInAssemblyList.Add(assembly);
             }
 
             return plugInAssemblyList;
+        }
+    }
+
+    class PluginLoadContext : AssemblyLoadContext
+    {
+        private AssemblyDependencyResolver _resolver;
+
+        public PluginLoadContext(string pluginPath)
+        {
+            _resolver = new AssemblyDependencyResolver(pluginPath);
+        }
+
+        protected override Assembly Load(AssemblyName assemblyName)
+        {
+            string assemblyPath = _resolver.ResolveAssemblyToPath(assemblyName);
+            if (assemblyPath != null)
+            {
+                return LoadFromAssemblyPath(assemblyPath);
+            }
+
+            return null;
+        }
+
+        protected override IntPtr LoadUnmanagedDll(string unmanagedDllName)
+        {
+            string libraryPath = _resolver.ResolveUnmanagedDllToPath(unmanagedDllName);
+            if (libraryPath != null)
+            {
+                return LoadUnmanagedDllFromPath(libraryPath);
+            }
+
+            return IntPtr.Zero;
         }
     }
 }
